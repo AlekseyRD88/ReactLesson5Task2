82dc1a4b880acc940985f94e5d5a2ee9c4951054
= <neymegen50@gmail.com>
2022-08-01 11:35:33 +0300

    message2

--- src/GoodButton.jsx
+++ src/GoodButton.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 class GoodButton extends React.Component {
   handleClick(event) {
-    console.log(event.target.textContent);
-    alert('Click me!');
+    alert(event.target.textContent);
   }
   render() {
     return(
